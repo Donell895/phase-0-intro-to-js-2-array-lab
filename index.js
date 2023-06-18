f84466aad69583176f659de8ea6f378c1c0d1f46
donell <donell.wambua@student.moringaschool.com>
2023-06-18 20:55:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+let cats=["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastcat() {
+    cats.pop()
+}
+ 
+function destructivelyRemovesFirstCat() {
+    cats.shift()
+}
+
+function appendcat(newcats) {
+    return newcats=[...cats,"Rose"]
+}
+
+function prependcat(latercats) {
+    return latercats=["Migan"]
+}
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
