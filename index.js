e3344b8ce0e32964ee22c08b84dd5ff89f3dabfd
donell <donell.wambua@student.moringaschool.com>
2023-06-18 23:00:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-// Write your solution here!
-let cats=["Milo","Otis","Garfield"]
+// index.js
 
-function destructivelyAppendCat(){
-    cats.push("Ralph")
-}
 
-function destructivelyPrependCat() {
-    cats.unshift("Bob")
-}
+// index.js
 
-function destructivelyRemoveLastcat() {
-    cats.pop()
-}
- 
-function destructivelyRemovesFirstCat() {
-    cats.shift()
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
 }
 
-function appendcat(newcats) {
-    return newcats=[...cats,"Rose"]
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
 }
 
-function prependcat(latercats) {
-    return latercats=["Migan"]
+function destructivelyRemoveLastCat() {
+  cats.pop();
 }
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
 function removeLastCat() {
-    return cats.slice(0,2)
+  return cats.slice(0, -1);
 }
 
 function removeFirstCat() {
-    return cats.slice(1)
+  return cats.slice(1);
 }
+
+
